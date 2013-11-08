955babf123964e9814bd9cb820ee66f09e62192b
Antonis Zisis <antonis.zisis@qualtrak.com>
2013-11-08 11:51:19 +0200

    Testing!  faulty

---
+++ tests/app/tenant/TenantControllerSpec.coffee
@@ -0,0 +1,27 @@
+﻿# CoffeeScript
+
+###
+/// <reference path="../../../static/lib/angular-1.2.0-rc.3/angular.js" />
+/// <reference path="../../../static/lib/angular-1.2.0-rc.3/angular-route.js" />
+/// <reference path="../../../static/lib/angular-1.2.0-rc.3/angular-resource.js" />
+/// <reference path="../../../static/lib/angular.treeview.js" />
+
+/// <reference path="../../lib/angular/angular-mocks.js" />
+
+/// <reference path="../../../static/app/tenant/app.js" />
+/// <reference path="../../../static/app/tenant/TenantController.js" />
+/// <reference path="../../../static/app/tenant/TenantService.js" />
+
+/// <reference path="../../../static/app/Index.html" />
+/// <reference path="../../../static/app/partials/assets.html" />
+###
+
+describe 'AngularJS controllers', ->
+    beforeEach module 'administratorApp'
+
+    describe 'LoadTenantCtrl', ->
+        it 'should load the tenant details', inject ($controller) ->
+            scope = {}
+            ctrl = $controller 'LoadTenantCtrl', {$scope: scope}
+
+            expect(scope.tenant.length).toBe 3
---
+++ tests/app/tenant/TenantControllerSpec.js
@@ -0,0 +1,35 @@
+﻿
+/*
+/// <reference path="../../../static/lib/angular-1.2.0-rc.3/angular.js" />
+/// <reference path="../../../static/lib/angular-1.2.0-rc.3/angular-route.js" />
+/// <reference path="../../../static/lib/angular-1.2.0-rc.3/angular-resource.js" />
+/// <reference path="../../../static/lib/angular.treeview.js" />
+
+/// <reference path="../../lib/angular/angular-mocks.js" />
+
+/// <reference path="../../../static/app/tenant/app.js" />
+/// <reference path="../../../static/app/tenant/TenantController.js" />
+/// <reference path="../../../static/app/tenant/TenantService.js" />
+
+/// <reference path="../../../static/app/Index.html" />
+/// <reference path="../../../static/app/partials/assets.html" />
+*/
+
+(function() {
+
+
+  describe('AngularJS controllers', function() {
+    beforeEach(module('administratorApp'));
+    return describe('LoadTenantCtrl', function() {
+      return it('should load the tenant details', inject(function($controller) {
+        var ctrl, scope;
+        scope = {};
+        ctrl = $controller('LoadTenantCtrl', {
+          $scope: scope
+        });
+        return expect(scope.tenant.length).toBe(3);
+      }));
+    });
+  });
+
+}).call(this);
